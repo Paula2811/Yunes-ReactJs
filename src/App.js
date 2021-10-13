6b704774bbf8bea055abf090e6144aedf3e2e309
Paula Yunes <pauliyunes94@gmail.com>
2021-10-12 22:41:22 -0300

    Creacion del Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import {Navbar,Container,Nav} from "react-bootstrap"
 import './App.css';
+import logo from './logo4.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="dark" variant="dark" ClassName="Navbar">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+              alt=""
+              src= {logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+              Sakura Libreria
+            </Navbar.Brand>
+            <Nav className="d-flex">
+              <Nav.Link href="">Inicio</Nav.Link>
+              <Nav.Link href="">Productos</Nav.Link>
+              <Nav.Link href="">Contacto</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </header>
     </div>
   );
