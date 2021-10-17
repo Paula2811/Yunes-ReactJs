8909ba12d7781008c17d26f964a08351ea61286b
Paula Yunes <pauliyunes94@gmail.com>
2021-10-17 19:50:55 -0300

    Incorporacion de un icono y un h1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import {Navbar,Container,Nav} from "react-bootstrap"
 import './App.css';
-import logo from './logo4.png';
+import {Menu} from './componentes/Navbar.jsx';
+import {Titulo} from './componentes/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar bg="dark" variant="dark" ClassName="Navbar">
-          <Container>
-            <Navbar.Brand href="#home">
-              <img
-              alt=""
-              src= {logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-              Sakura Libreria
-            </Navbar.Brand>
-            <Nav className="d-flex">
-              <Nav.Link href="">Inicio</Nav.Link>
-              <Nav.Link href="">Productos</Nav.Link>
-              <Nav.Link href="">Contacto</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+        <Menu />
+        <Titulo/>
       </header>
     </div>
   );
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,34 @@
+import logo from './logo4.png';
+import {Navbar,Container,Nav} from 'react-bootstrap';
+import {BsFillCartCheckFill} from 'react-icons/bs'
+//Navbar
+export function Menu() {
+    return(
+        <Navbar bg="dark" variant="dark" ClassName="Navbar">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src= {logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />
+                    Sakura Libreria
+                </Navbar.Brand>
+                <Nav className="d-flex">
+                    <Nav.Link href="">Inicio</Nav.Link>
+                    <Nav.Link href="">Productos</Nav.Link>
+                    <Nav.Link href="">Contacto</Nav.Link>
+                    <Nav.Link href=""><Icon/></Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+//Icono
+function Icon (){
+    return(
+        <BsFillCartCheckFill/>
+    )
+}
