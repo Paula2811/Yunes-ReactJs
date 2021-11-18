0c770b39acc17aefdf31266a369f9fe8288c2670
Paula Yunes <pauliyunes94@gmail.com>
2021-11-17 21:46:13 -0300

    Desafio 13.2

--- src/componentes/Cart.jsx
+++ src/componentes/Cart.jsx
@@ -2,58 +2,72 @@ import {Card, Button, Form, Alert} from 'react-bootstrap';
 import {Link} from "react-router-dom"
 import { useState} from 'react'
 import { useCartContext } from '../context/CartContext';
-import firebase from 'firebase';
-import 'firebase/firestore';
 import { getFirestore } from '../services/getFirestore';
 import '../css/cart.css'
+import firebase from "firebase"
+import 'firebase/firestore'
 
 const Cart = () =>{
     const {cartList, removeItem, removeCart, cartTotal} = useCartContext()
-    const [form, setForm]= useState({name:"", email:"",number:""})
-    const [ordenId, setOrdenId] = useState("");
+    const [formData, setFormData]= useState({name:"", email:"",number:""})
+    const [idOrder, setIdOrder] = useState("");
 
     const generarOrden = (e)=>{
         e.preventDefault()
 
         let orden = {}
-        orden.date = firebase.firestore.Timestamp.fromDate(new Date())
-        orden.buyer = form
-        orden.total = cartTotal
-        orden.items = cartList.map(itemAgregado =>{
-            const id = itemAgregado.item.id
-            const title = itemAgregado.item.title
-            const total = itemAgregado.item.price * itemAgregado.cantidad
-            return {id, title, total}
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date());    
+        orden.buyer = formData
+        orden.total = cartTotal;
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.id;
+            const title = cartItem.title;
+            const price = cartItem.price * cartItem.cantidad;
+            
+            return {id, title, price}   
         })
+
         
-        const db = getFirestore()
-        db.collection("ordenes").add(orden)
-        .then(resp => setOrdenId(resp.id))
-        .catch(error => alert(error))
-        .finally(() =>removeCart())
+        const dbQuery = getFirestore()
 
-        const newStock = db.collection("items").where(firebase.firestore.FieldPath.documentId(),"in",cartList.map(newStock => newStock.item.id))
+        dbQuery.collection('orders').add(orden)
+        .then(resp => setIdOrder(resp.id))
+        .catch(err=> console.log(err))
+        .finally(()=> setFormData({
+            name:'',
+            phone:'',
+            email: ''
+        }))
 
-        const batch = db.batch()
 
-        newStock.get()
-        .then(resp =>{
-            resp.docs.forEach(docSnapshot =>{
+        const itemsToUpdate = dbQuery.collection('items').where(
+            firebase.firestore.FieldPath.documentId(), 'in', cartList.map(i=> i.id)
+        )
+    
+        const batch = dbQuery.batch();
+        
+    
+        itemsToUpdate.get()
+        .then( collection=>{
+            collection.docs.forEach(docSnapshot => {
                 batch.update(docSnapshot.ref, {
-                    stock: docSnapshot.data().stock - cartList.find(newId => newId.item.id === docSnapshot.id).cantidad
+                    stock: docSnapshot.data().stock - cartList.find(item => item.id === docSnapshot.id).cantidad
+                })
+            })
+    
+            batch.commit().then(res =>{
+                console.log('resultado batch:', res)
             })
         })
-        batch.commit()
-            .then(resp => {console.log("Stock actualizado",resp)})
-        })
+
     }
-    const cambio = (e) =>{
-        setForm({
-            ...setForm, 
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData, 
             [e.target.name]: e.target.value
         })
     }
-
+    console.log(formData)
     return (
         <div className="form">
             <div> 
@@ -72,7 +86,6 @@ const Cart = () =>{
                                 <Button variant="primary" onClick={() => removeItem(itemAgregado.product.id)} className="cartButton">Eliminar Producto</Button>
                                 <Card.Text>
                                 Subtotal: $ {itemAgregado.cantidad * itemAgregado.product.price}
-                                {console.log(cartTotal)}
                                 </Card.Text>
                         </Card.Body>
                     </Card>    
@@ -87,7 +100,7 @@ const Cart = () =>{
             </div>
         {cartList.length
         ? <Button  onClick={() => removeCart()} className="cart">Vaciar carrito</Button>
-        :ordenId ===""
+        :idOrder ===""
             ?<div>
                 <b>El carrito está vacío</b>
                 <Link to="/"> 
@@ -97,7 +110,7 @@ const Cart = () =>{
             :<Alert variant="success">
                 <Alert.Heading>Gracias por su compra!</Alert.Heading>
                 <p>
-                Tu codigo de compra es {ordenId}
+                Tu codigo de compra es {idOrder}
                 </p>
                 <hr />
                 <Link to="/" className="mb-0"> 
@@ -105,19 +118,19 @@ const Cart = () =>{
                 </Link>
             </Alert>
         }
-        <Form onSubmit={generarOrden} onChange={cambio} className="cart">
+        <Form onSubmit={generarOrden} onChange={handleChange} className="cart">
             <legend className="form-legend">Ingresá tus datos</legend>
             <Form.Group className="mb-3">
                 <Form.Label>Nombre</Form.Label>
-                <Form.Control type="text" placeholder="Enter name" value={form.name}/>
+                <Form.Control type="text" placeholder="Enter name" value={formData.name}/>
             </Form.Group>
             <Form.Group className="mb-3">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" value={form.email}/>
+                <Form.Control type="email" placeholder="Enter your email" value={formData.email}/>
             </Form.Group>
             <Form.Group className="mb-3">
             <Form.Label>Telefono</Form.Label>
-                <Form.Control type="text" placeholder="Enter your phone number" value={form.number} />
+                <Form.Control type="text" placeholder="Enter your phone number" value={formData.number} />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Terminar la compra!
